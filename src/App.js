e8918e683cb39b674199dd78b13aa5092522d16d
Jenna Perez <jmsp43@gmail.com>
2023-05-24 15:50:17 -0400

    sign in with jwt functional

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import AuthPage from "./components/pages/Auth/AuthPage";
 import NewOrderPage from "./components/pages/NewOrder/NewOrderPage";
 import OrderHistoryPage from "./components/pages/OrderHistory/OrderHistoryPage";
 import NavBar from "./components/NavBar";
+import { getUser } from './utilities/users-service.js'
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(getUser());
 
   return (
     <main className="App">
